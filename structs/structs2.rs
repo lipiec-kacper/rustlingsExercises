3cfca6f462b7657311260d9c25cef23ee3777cba
lipiec-kacper <lipiec2003kacper@gmail.com>
2024-05-24 14:11:10 +0200

    primitive types and structures

--- structs/structs2.rs
+++ structs/structs2.rs
@@ -5,7 +5,6 @@
 // Execute `rustlings hint structs2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 #[derive(Debug)]
 struct Order {
@@ -16,6 +15,18 @@ struct Order {
     made_by_email: bool,
     item_number: u32,
     count: u32,
+}
+
+fn myownOrder(definedOrder : &Order) -> Order {
+    Order{
+    name : String::from("Hacker in Rust"),
+    year : definedOrder.year,
+    made_by_phone : definedOrder.made_by_phone,
+    made_by_mobile : definedOrder.made_by_mobile,
+    made_by_email : definedOrder.made_by_email,
+    item_number : definedOrder.item_number,
+    count : 1,
+    }
 }
 
 fn create_order_template() -> Order {
@@ -38,7 +49,7 @@ mod tests {
     fn your_order() {
         let order_template = create_order_template();
         // TODO: Create your own order using the update syntax and template above!
-        // let your_order =
+        let your_order = myownOrder(&order_template);
         assert_eq!(your_order.name, "Hacker in Rust");
         assert_eq!(your_order.year, order_template.year);
         assert_eq!(your_order.made_by_phone, order_template.made_by_phone);
